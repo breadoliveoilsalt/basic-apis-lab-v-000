9c212bd238a5c9027f0975090ebf3601e59bb565
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-04-09 18:32:23 -0400

    resolve merge conflict

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,6 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+
     begin
       @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
           req.params['client_id'] = "REPLACE WITH STRING"
@@ -27,6 +28,6 @@ class RepositoriesController < ApplicationController
     end
 
     render 'search'
-  end
 
+  end
 end
