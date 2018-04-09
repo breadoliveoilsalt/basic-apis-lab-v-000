a8e5fca2bb814b4905036b1857d6d30f5d03b5e3
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-04-09 18:28:13 -0400

    fixing token matters

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,14 +7,9 @@ class RepositoriesController < ApplicationController
   def github_search
     begin
       @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-          # req.params['client_id'] = 'Iv1.c9812a8124fe29a9' #need to put actual id and secret in as STRINGS
-          # req.params['client_secret'] = '289897a0584fb7be25ac5fcf1a6da29ee989da44'
-          # req.params['v'] = '20160201'
           req.params['client_id'] = "REPLACE WITH STRING"
           req.params['client_secret'] = "REPLACE WITH STRING"
-          # req.params['access_token'] = '5a3460318687a87db820bef274b6e2358dda640b'
           req.params['q'] = params[:query]
-          # req.params['query'] = 'coffee shop'
 
       end
 
